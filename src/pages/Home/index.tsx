c9c3837c810be62575dbf9b855f9cac594698933
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-08-15 22:39:29 -0300

    adicionando validação reset e typescript ao formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,10 @@
 import { Play } from 'phosphor-react'
+// React-hook-form e necessário para integração do useForm com validadores externos
+import { useForm } from 'react-hook-form'
+// O zod é um validador de formularios que pode ser integrado ao useForm atravéz do react-hook-form
+import { zodResolver } from '@hookform/resolvers/zod'
+// Importando todas as funções de validação de zod e atribuindo a uma unica variável. A lib Zod não temum export Default, por essa razão isso é necessário.
+import * as zod from 'zod'
 import {
   CountdownContainer,
   FormContainer,
@@ -9,16 +15,65 @@ import {
   TaskInput,
 } from './styles'
 
+// Validação de um objeto (por essa razão zod.object({}))
+// Definindo configurações de validação do objeto para submit
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no minimo 65 minútos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minútos.'),
+})
+
+/*
+  Sempre que se quer referênciar uma variável JS dentro do TS é necessário utilizar
+  o typeof. Dessa forma é possível criar um tipo por inferência automático.
+
+  O zod tem a capacidade de inferir o tipo a partir da validação, sem a necessidade
+  da criação de uma interface para tipar os inputs do formulário como na linha seguinte:
+  
+  interface NewCycleFormData {
+    task: string
+    minutesAmount: number
+  }
+
+  basta utilizar zod.infer<typeof TIPO DO SCHEMA>
+*/
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  /*
+    Observando o valor do input task em tempo real sem a necessidade do mesmos ser
+    um componente controlado. Isso melhora a performance da aplicação.
+  */
+  const task = watch('task')
+
+  // Clean code - Melhora a legibilidade do código sem afetar o mesmo negativamente
+  const isSubmiteDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -36,6 +91,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -49,7 +105,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmiteDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
